0a370b8e2ab958814573596f7d0d715caf43be19
RUPENDER SAHARAN <rupenderer@gmail.com>
2026-08-18 17:15:36 +0530

    Time: 3 ms (14.49%) | Memory: 44.7 MB (44.67%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -4,7 +4,6 @@ class Solution {
 
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-
         backtrack(result, "", 0, 0, n);
 
         return result;
